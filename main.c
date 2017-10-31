af633009e13553a7cff6e37582218251605fb5d4
luchk <30504944+luchk@users.noreply.github.com>
2017-10-31 23:25:53 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int t[24] =  {11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 14, 12, 11, 10, 10, 9, 9};
+    int all = 0, i;
+    int a = 0;
+    float avarage;
+    for (i = -1; i <=23; ++i) {
+        all = all + t[i];
+    }
+  //  printf("%x \n", all);
+    all =0 ;
+    while (a <= 23) {
+        all +=t[a];
+       // printf("%d    %d \n",a, all);
+        a+=1;
+    }
+
+    avarage = all / 24;
+    printf("ts= %f", avarage);
+    return 0;
+}
